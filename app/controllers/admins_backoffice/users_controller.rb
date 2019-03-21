652bb561092f7684bb30c5057f106683fbfdf675
warleyhenrique <warley.henrique93@gmail.com>
2019-03-21 16:42:20 +0000

    Update users in admins_backoffice

--- app/controllers/admins_backoffice/users_controller.rb
+++ app/controllers/admins_backoffice/users_controller.rb
@@ -8,5 +8,18 @@ class AdminsBackoffice::UsersController < AdminsBackofficeController
   end
 
   def update
+    if params[:user][:password].blank?
+      params[:user].extract!(:password, :password_confirmation)
+    end
+
+    @user = User.find(params[:id])
+    params_user = params.require(:user).permit(:email, :password, :password_confirmation)
+
+    if @user.update(params_user)
+      redirect_to admins_backoffice_users_path, notice:"Usuario atualizado com sucesso"
+    else
+      render :edit
+    end
   end
 end
+
