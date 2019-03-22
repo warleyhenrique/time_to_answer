d7bd71dd523655e132760cdd1adf860e7dde138e
warleyhenrique <warley.henrique93@gmail.com>
2019-03-22 00:01:45 -0300

    Refatoring admin and user controllers

--- app/controllers/admins_backoffice/users_controller.rb
+++ app/controllers/admins_backoffice/users_controller.rb
@@ -1,25 +1,36 @@
 class AdminsBackoffice::UsersController < AdminsBackofficeController
+  before_action :verify_params, only: [:update]
+  before_action :set_user, only: [:edit, :update]
+
   def index
     @users = User.all
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    if params[:user][:password].blank?
-      params[:user].extract!(:password, :password_confirmation)
-    end
-
-    @user = User.find(params[:id])
-    params_user = params.require(:user).permit(:email, :password, :password_confirmation)
-
     if @user.update(params_user)
       redirect_to admins_backoffice_users_path, notice:"#{@user.email} atualizado com sucesso!"
     else
       render :edit
     end
+  end
+
+  private
+
+  def verify_params
+    if params[:user][:password].blank?
+      params[:user].extract!(:password, :password_confirmation)
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def params_user
+    params_user = params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
 
